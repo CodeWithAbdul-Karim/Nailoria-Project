7448f342f5e91a453bc838d1edc19ce8526f1b2e
Abdul-karim <abdullahasifmemon007@gmail.com>
2026-05-01 03:25:30 +0500

    contact page complete

--- index.js
+++ index.js
@@ -1,23 +1,110 @@
+
 $(document).ready(function () {
+    // CONTACT FORM VALIDATIONS
+    $("#nailoria-form").submit(function (e) {
+        e.preventDefault();
+        let name = $("#name").val().trim();
+        let email = $("#email").val().trim();
+        let password = $("#password").val().trim();
+        let comment = $("#comment").val().trim();
+        let isValid = true
+        $(".error").text("");
 
-    // TOGGLER MENU
-    $(".navbar-collapse").on("show.bs.collapse", function () {
-        $(".header-menu").css("background-color", "white");
-    });
+        if (name === "") {
+            $("#name").next(".error").text("Please enter your name.");
+            $("#name").css("border", "1px solid #dc3545");
+            isValid = false;
+        }
+        if (email === "") {
+            $("#email").next(".error").text("Please enter a valid email.");
+            $("#email").css("border", "1px solid #dc3545");
+            isValid = false;
+        }
+        if (password === "") {
+            $("#password").next(".error").text("Please enter your email password.");
+            $("#password").css("border", "1px solid #dc3545");
+            isValid = false;
+        } else if (password.length < 6) {
+            $("#password").next(".error").text("Minimum 6 letter required.");
+            $("#password").css("border", "1px solid #dc3545");
+            isValid = false;
+        }
+        if (comment === "") {
+            $("#comment").next(".error").text("Message must be 10-1000 characters.");
+            $("#comment").css("border", "1px solid #dc3545");
+            isValid = false;
+        } else if (comment.length < 10) {
+            $("#comment").next(".error").text("Minimum 10 characters required.");
+            $("#comment").css("border", "1px solid #dc3545");
+            isValid = false;
+        }
 
-    $(".navbar-collapse").on("hide.bs.collapse", function () {
-        $(".header-menu").css("background-color", "");
-    });
+        if (isValid) {
+            $("#nailoria-form")[0].reset();
 
-    // NAVBAR COLOR CHANGE ON SCROLL
-    window.addEventListener("scroll", function () {
-        const navbar = document.querySelector(".header-menu");
+            $("#successAlert").removeClass("d-none");
 
-        if (window.scrollY > 50) {
-            navbar.classList.add("nav-scrolled");
-        } else {
-            navbar.classList.remove("nav-scrolled");
+            setTimeout(function () {
+                $("#successAlert").addClass("d-none");
+            }, 5000);
         }
     });
 
+ 
+
+
+    // FEEDBACK FORM VALIDATIONS
+    $("#feedback-form").submit(function (e) {
+        e.preventDefault();
+        let name = $("#f-name").val().trim();
+        let email = $("#f-email").val().trim();
+        let rating = $("#rating").val();
+        let feedback = $("#feedback").val().trim();
+        let isValid = true;
+        $(".error").text("");
+
+        if (name === "") {
+            $("#f-name").next(".error").text("Please enter your name.");
+            $("#f-name").css("border", "1px solid #dc3545");
+            isValid = false;
+        }
+        if (email === "") {
+            $("#f-email").next(".error").text("Please enter a valid email.");
+            $("#f-email").css("border", "1px solid #dc3545");
+            isValid = false;
+        }
+        if (!rating) {
+            $("#rating").next(".error").text("Please select a rating.");
+            $("#rating").css("border", "1px solid #dc3545");
+            isValid = false;
+        }
+        if (feedback === "") {
+            $("#feedback").next(".error").text("Feedback must be 10-1000 characters.");
+            $("#feedback").css("border", "1px solid #dc3545");
+            isValid = false;
+        } else if (feedback.length < 10) {
+            $("#feedback").next(".error").text("Minimum 10 characters required.");
+            $("#feedback").css("border", "1px solid #dc3545");
+            isValid = false;
+        }
+
+        // RESET FORM FIELDS AND SUCCESSFUL ALERT
+        if (isValid) {
+            $("#feedback-form")[0].reset();
+
+            $("#f-successAlert").removeClass("d-none");
+
+            setTimeout(function () {
+                $("#f-successAlert").addClass("d-none");
+            }, 5000);
+        }
+    });
+
+    // REAL TIME ERROR
+       $("input, textarea, select").on("input", function () {
+        if ($(this).val().trim() !== "") {
+            $(this).siblings(".error").text("");
+            $(this).css("border", "");
+        }
+    });
 });
